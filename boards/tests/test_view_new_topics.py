bb7d90e2b4b3269213ea139dc1ecd5a2d53ddd20
Sadh <sadh.ibna.rahmat@gmail.com>
2018-03-07 16:48:35 +0600

    my account added

--- boards/tests/test_view_new_topics.py
+++ boards/tests/test_view_new_topics.py
@@ -6,60 +6,11 @@ from ..views import home, board_topics, new_topic
 from ..models import Board, Topic, Post
 from ..forms import NewTopicForm
 
-# Create your tests here.
-
-class HomeTests(TestCase):
-    def setUp(self):
-        self.board = Board.objects.create(name='Django', description='Django Board.')
-        url = reverse('home')
-        self.response = self.client.get(url)
-
-    def test_home_view_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
-
-    def test_home_url_resolves_home_view(self):
-        view = resolve('/')
-        self.assertEquals(view.func, home)
-
-    def test_home_view_has_link_to_topics_page(self):
-        board_topics_url = reverse('board_topics',kwargs={'pk':self.board.pk})
-        self.assertContains(self.response,'href="{0}"'.format(board_topics_url))
-
-
-class BoardTopicTests(TestCase):
-    def setUp(self):
-        Board.objects.create(name='Django', description='Django Board.')
-
-    def test_board_topics_view_success_status_code(self):
-        url = reverse('board_topics',kwargs={'pk':1})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
-
-    def test_board_topics_view_not_found_status_code(self):
-        url = reverse('board_topics', kwargs={'pk': 99})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 404)
-
-
-    def test_borad_topics_url_resolves_board_topics_view(self):
-        view = resolve('/boards/1/')
-        self.assertEquals(view.func,board_topics)
-
-    def test_board_topics_view_contains_navigation_link(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
-        homepage_url = reverse('home')
-        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
-
-        response = self.client.get(board_topics_url)
-
-        self.assertContains(response, 'href="{0}"'.format(homepage_url))
-        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
-
-
 class NewTopicTests(TestCase):
     def setUp(self):
         Board.objects.create(name='Django', description='Django Board.')
         User.objects.create_user(username='sadh', email='sadh@rahmat.com', password='123')
+        self.client.login(username='sadh',password='123')
 
     def test_csrf(self):
         url = reverse('new_topic', kwargs={'pk': 1})
@@ -128,3 +79,13 @@ class NewTopicTests(TestCase):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(new_topic_url)
         self.assertContains(response, 'href="{0}"'.format(board_topics_url))
+
+class LoginRequiredNewTopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django Board.')
+        self.url = reverse('new_topic', kwargs={'pk':1})
+        self.response = self.client.get(self.url)
+
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, "{login_url}?next={url}".format(login_url=login_url,url=self.url))
--- bondhushova/urls.py
+++ bondhushova/urls.py
@@ -21,11 +21,13 @@ from django.contrib.auth import views as auth_views
 from accounts import views as accounts_views
 from boards import views
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.BoardListView.as_view(), name='home'),
     url(r'^signup/$',accounts_views.signup,name='signup'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^login/$',auth_views.login,
         {'template_name':'login.html'},name='login'),
+    url(r'^settings/account/$', accounts_views.UserUpdateView.as_view(),
+        {'template_name': 'my_account.html'}, name='my_account'),
     url(r'^settings/password/$', auth_views.password_change,
         {'template_name': 'password_change.html'}, name='password_change'),
     url(r'^settings/password/done/$', auth_views.password_change_done,
@@ -41,7 +43,11 @@ urlpatterns = [
         {'template_name':'password_reset_confirm.html'}, name='password_reset_confirm'),
     url(r'^reset/complete/$', auth_views.password_reset_complete,
         {'template_name':'password_reset_complete.html'}, name='password_reset_complete'),
-    url(r'^boards/(?P<pk>\d+)/$',views.board_topics,name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/$',views.TopicListView.as_view(),name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
+        views.PostUpdateView.as_view(), name='edit_post'),
     url(r'^admin/', admin.site.urls),
 ]
